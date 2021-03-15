375826f8055530b7f58b1cbea9cbd6f3b69c31a6
DenisRudakov0 <denis.rudakov.2000.14@gmail.com>
2021-03-15 11:17:12 +0300

    modification_func_dec_A-Z

--- zd.py
+++ zd.py
@@ -3,7 +3,10 @@ from math import *
 def dec_2_fact_string(nb):
     factorial_string, i = '', 1
     while nb != 0:
-        factorial_string += str(nb % i)
+        if nb % i > 9:
+            factorial_string += chr(65 + (nb % i) - 10)
+        else:
+            factorial_string += str(nb % i)
         nb = nb // i
         i += 1
     return factorial_string[::-1]
@@ -11,12 +14,13 @@ def dec_2_fact_string(nb):
 def fact_string_2_dec(string):
     string, nb = [int(string[i]) for i in range(len(string))], 0
     for i in range(len(string)):
-        print(nb * (len(string) - i) + (string[i]))
         nb = nb * (len(string) - i) + (string[i])
     return nb
 
-print(dec_2_fact_string(463))
-print(fact_string_2_dec("341010"))
+print(dec_2_fact_string(36288000))
+print(fact_string_2_dec("010000000000"))
+
+
 
 
 
