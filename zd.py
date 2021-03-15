b4961268b4b6fe3d102730e5ae897ba4bcb1a2b6
DenisRudakov0 <denis.rudakov.2000.14@gmail.com>
2021-03-15 10:25:18 +0300

    Add_fact_string_2_dec

--- zd.py
+++ zd.py
@@ -1,3 +1,5 @@
+from math import *
+
 def dec_2_fact_string(nb):
     factorial_string, i = '', 1
     while nb != 0:
@@ -7,9 +9,14 @@ def dec_2_fact_string(nb):
     return factorial_string[::-1]
     
 def fact_string_2_dec(string):
-    pass
+    string, nb = [int(string[i]) for i in range(len(string))], 0
+    for i in range(len(string)):
+        print(nb * (len(string) - i) + (string[i]))
+        nb = nb * (len(string) - i) + (string[i])
+    return nb
 
 print(dec_2_fact_string(463))
+print(fact_string_2_dec("341010"))
 
 
 
